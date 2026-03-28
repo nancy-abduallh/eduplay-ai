c86355555f6eba68606c1dae611f3cb96a9393c9
Nancy Abduallh <142435484+nancy-abduallh@users.noreply.github.com>
2026-03-28 18:26:12 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,30 +6,9 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::get('locale/{locale}', function ($locale) {
-    if (!in_array($locale, ['en', 'ar'])) {
-        abort(400);
-    }
-    session(['locale' => $locale]);
-    app()->setLocale($locale);
-    return redirect()->to(LaravelLocalization::getLocalizedURL($locale, null, [], true));
-})->name('locale.set');
 
-// Routes for Arabic (with prefix)
 Route::group([
-    'prefix' => 'ar',
-    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
-], function () {
-    Route::get('/', [HomeController::class, 'index'])->name('home.ar');
-    Route::post('/fetch/start', [FetchController::class, 'start'])->name('fetch.start.ar');
-    Route::get('/fetch/progress/{session}', [FetchController::class, 'progress'])->name('fetch.progress.ar');
-    Route::get('/fetch/status/{session}', [FetchController::class, 'status'])->name('fetch.status.ar');
-    Route::get('/courses', [CourseController::class, 'index'])->name('courses.index.ar');
-    Route::get('/courses/{course}', [CourseController::class, 'show'])->name('courses.show.ar');
-});
-
-// Routes for English (no prefix)
-Route::group([
+    'prefix'     => LaravelLocalization::setLocale(),
     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'],
 ], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
