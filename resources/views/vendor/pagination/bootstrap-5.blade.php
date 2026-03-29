2346b1a4b86d03cca5dfac7341777029cb98ac3e
Nancy Abduallh <142435484+nancy-abduallh@users.noreply.github.com>
2026-03-29 15:45:34 +0200

    Refactor pagination view for Bootstrap 5

--- resources/views/vendor/pagination/bootstrap-5.blade.php
+++ resources/views/vendor/pagination/bootstrap-5.blade.php
@@ -1,86 +1,105 @@
 @if ($paginator->hasPages())
 <div class="pagination-wrapper">
-    <ul class="pagination">
+<ul class="pagination">
 
-        {{-- Previous --}}
-        @if ($paginator->onFirstPage())
-            <li class="page-item disabled">
-                <span class="page-link"><i class="fas fa-chevron-left"></i></span>
-            </li>
-        @else
-            <li class="page-item">
-                <a class="page-link" href="{{ $paginator->previousPageUrl() }}">
-                    <i class="fas fa-chevron-left"></i>
-                </a>
-            </li>
-        @endif
-
-
-        @php
-            $current = $paginator->currentPage();
-            $last = $paginator->lastPage();
-            $start = max($current - 2, 1);
-            $end = min($current + 2, $last);
-        @endphp
+{{-- Previous --}}
+@if ($paginator->onFirstPage())
+<li class="page-item disabled">
+<span class="page-link"><i class="fas fa-chevron-left"></i></span>
+</li>
+@else
+<li class="page-item">
+<a class="page-link" href="{{ $paginator->previousPageUrl() }}">
+<i class="fas fa-chevron-left"></i>
+</a>
+</li>
+@endif
 
 
-        {{-- First Page --}}
-        @if ($start > 1)
-            <li class="page-item">
-                <a class="page-link" href="{{ $paginator->url(1) }}">1</a>
-            </li>
+@php
+$current = $paginator->currentPage();
+$last = $paginator->lastPage();
 
-            @if ($start > 2)
-                <li class="page-item disabled">
-                    <span class="page-link">...</span>
-                </li>
-            @endif
-        @endif
+$window = 5;
+$half = floor($window / 2);
 
+$start = $current - $half;
+$end = $current + $half;
 
-        {{-- Pages Around Current --}}
-        @for ($page = $start; $page <= $end; $page++)
-            @if ($page == $current)
-                <li class="page-item active">
-                    <span class="page-link">{{ $page }}</span>
-                </li>
-            @else
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->url($page) }}">{{ $page }}</a>
-                </li>
-            @endif
-        @endfor
+if ($start < 1) {
+    $start = 1;
+    $end = min($window, $last);
+}
+
+if ($end > $last) {
+    $end = $last;
+    $start = max($last - $window + 1, 1);
+}
+@endphp
 
 
-        {{-- Last Page --}}
-        @if ($end < $last)
+{{-- Show first page if hidden --}}
+@if ($start > 1)
+<li class="page-item">
+<a class="page-link" href="{{ $paginator->url(1) }}">1</a>
+</li>
 
-            @if ($end < $last - 1)
-                <li class="page-item disabled">
-                    <span class="page-link">...</span>
-                </li>
-            @endif
-
-            <li class="page-item">
-                <a class="page-link" href="{{ $paginator->url($last) }}">{{ $last }}</a>
-            </li>
-
-        @endif
+@if ($start > 2)
+<li class="page-item disabled">
+<span class="page-link">...</span>
+</li>
+@endif
+@endif
 
 
-        {{-- Next --}}
-        @if ($paginator->hasMorePages())
-            <li class="page-item">
-                <a class="page-link" href="{{ $paginator->nextPageUrl() }}">
-                    <i class="fas fa-chevron-right"></i>
-                </a>
-            </li>
-        @else
-            <li class="page-item disabled">
-                <span class="page-link"><i class="fas fa-chevron-right"></i></span>
-            </li>
-        @endif
+{{-- Sliding window pages --}}
+@for ($page = $start; $page <= $end; $page++)
+@if ($page == $current)
+<li class="page-item active">
+<span class="page-link">{{ $page }}</span>
+</li>
+@else
+<li class="page-item">
+<a class="page-link" href="{{ $paginator->url($page) }}">
+{{ $page }}
+</a>
+</li>
+@endif
+@endfor
 
-    </ul>
+
+{{-- Show last page if hidden --}}
+@if ($end < $last)
+
+@if ($end < $last - 1)
+<li class="page-item disabled">
+<span class="page-link">...</span>
+</li>
+@endif
+
+<li class="page-item">
+<a class="page-link" href="{{ $paginator->url($last) }}">
+{{ $last }}
+</a>
+</li>
+
+@endif
+
+
+{{-- Next --}}
+@if ($paginator->hasMorePages())
+<li class="page-item">
+<a class="page-link" href="{{ $paginator->nextPageUrl() }}">
+<i class="fas fa-chevron-right"></i>
+</a>
+</li>
+@else
+<li class="page-item disabled">
+<span class="page-link"><i class="fas fa-chevron-right"></i></span>
+</li>
+@endif
+
+
+</ul>
 </div>
 @endif
